ca7b4e0df46697b58c55d299ab7df75631bcaa6a
Rex Yu <yu19910513@gmail.com>
2022-02-23 21:27:24 -0800

    function of account option implemented

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -5,7 +5,11 @@ function accountList() {
     }).then(function (response) {
         return response.json();
     }).then(function (data) {
-    console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            const option = document.createElement('option');
+            option.innerHTML = data[i].name;
+            document.querySelector('#accountList').appendChild(option);
+        }
     });
 
 };
