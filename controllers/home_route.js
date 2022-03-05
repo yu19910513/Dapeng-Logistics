42551b33c2406b59ae3f8bae6574ae6ebdabfdc1
Rex Yu <yu19910513@gmail.com>
2022-03-05 15:15:27 -0800

    auto receving

--- controllers/home_route.js
+++ controllers/home_route.js
@@ -270,6 +270,68 @@ router.get('/admin_receiving', withAuth, async (req, res) => {
 
 });
 
+router.get('/admin_receiving_main', withAuth, async (req, res) => {
+  try {
+    const boxData = await Box.findAll({
+      where: {
+        status:0
+      },
+      attributes: [
+        'id',
+        'box_number',
+        'description',
+        'cost',
+        'requested_date',
+        'received_date',
+        'shipped_date',
+        'order',
+        'qty_per_box',
+        'length',
+        'width',
+        'height',
+        'weight',
+        'volume',
+        'status',
+        'location',
+        'sku',
+        'file',
+        'file_2',
+      ],
+      include: [
+        {
+          model: Batch,
+          attributes: [
+            'asn',
+            'pending_date',
+            'total_box'
+          ]
+        },
+        {
+          model: Account,
+          attributes: [
+            'name'
+          ]
+        },
+        {
+          model: User,
+          attributes: [
+            'id',
+            'name',
+            'email',
+            'wechat'
+          ]
+        }
+      ]
+    });
+    const boxes = boxData.map(box => box.get({ plain: true }));
+    res.render('receiving_main', { boxes, loggedIn: true, admin: req.session.admin, name: req.session.name });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+});
+
 //barcode generation per batch id
 router.get('/batch/:id', withAuth, async (req, res) => {
   try {
