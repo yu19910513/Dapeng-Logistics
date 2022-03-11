9a29e7d2c28c864b916934c6ceaf600060fbabed
Rex Yu <yu19910513@gmail.com>
2022-03-10 20:25:11 -0800

    frame work

--- controllers/home_route.js
+++ controllers/home_route.js
@@ -397,6 +397,14 @@ router.get('/admin_receiving_main', withAuth, async (req, res) => {
 
 });
 
+router.get('/admin_move_main', withAuth, (req, res) => {
+  try {
+    res.render('dynamic_move', {loggedIn: true, admin: req.session.admin, name: req.session.name });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 router.get('/box_location', withAuth, async (req, res) => {
   try {
