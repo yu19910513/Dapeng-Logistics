1bbc955d5d1e83bd51c22f5a0987261833a7b2b9
Rex Yu <yu19910513@gmail.com>
2022-03-16 00:11:01 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL)
+  ? new Sequelize({
+    hostname: 'dapeng-logistics2022.ccff9y2ps46r.us-east-1.rds.amazonaws.com',
+    username: 'dapeng_logistics',
+    password: 'Ma09dapeng01',
+    port: 3306
+  })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
