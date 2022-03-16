fe5163388427484bcaf6f305fff83256640202f3
Rex Yu <yu19910513@gmail.com>
2022-03-15 21:29:26 -0700

    DATABASE_URL

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
