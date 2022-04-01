e6f88fe43cd824168e151018789bacfffd75a1be
Rex Yu <yu19910513@gmail.com>
2022-04-01 02:16:55 -0700

    3' git push

--- controllers/api/account-route.js
+++ controllers/api/account-route.js
@@ -5,7 +5,7 @@ const {withAuth, adminAuth} = require('../../utils/auth');
 //create new account
 router.post('/', withAuth, (req, res) => {
     Account.create({
-        user_id: req.body.user_id,
+        user_id: req.session.user_id,
         name: req.body.name,
         prefix: req.body.prefix
     }).then(dbAccounttData => res.json(dbAccounttData))
