43a8d44427085a0ea6d72a66912f99e48993ff20
Rex Yu <yu19910513@gmail.com>
2022-04-13 00:17:47 -0700

    amazon client

--- public/account/javascript/home.js
+++ public/account/javascript/home.js
@@ -4,7 +4,7 @@ function number_item () {
     var table, tr, td, txtValue;
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
+    for (i = 1; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[10];
       if (td) {
         txtValue = td.textContent || td.innerText;
@@ -48,59 +48,3 @@ function saveAccount() {
 };
 document.querySelector("#account_selection").addEventListener("click", saveAccount);
 //////////// amazon code from here ////////////////////
-// var containerMap = new Map();
-// var skuMap = new Map();
-// var itemNumberArr = [];
-// var containerNumberArr = [];
-// var receivedCount = 0;
-// var requestedCount = 0;
-// function allItem() {
-//     fetch(`/api/item/allItem/${account_id}`, {
-//       method: 'GET'
-//     }).then(function (response) {
-//       return response.json();
-//     }).then(function (data) {
-//         console.log(data);
-//       //sort items by item_number
-//       const item_data = data.reduce((r, a) => {
-//         r[a.item_number] = r[a.item_number] || [];
-//         r[a.item_number].push(a);
-//         return r;
-//       }, Object.create(null));
-//       //collection all skus (item_number)
-//       for (let k = 0; k < data.length; k++) {
-//         const item = data[k].item_number;
-//         if (!itemNumberArr.includes(item)) {
-//           itemNumberArr.push(item);
-//         }
-//       };
-//       itemNumberArr.forEach(number => {
-//         skuMap.set(number, item_data[number])
-//       });
-//       // sort items by container
-//       const container_data = data.reduce((r, a) => {
-//         r[a.container.container_number] = r[a.container.container_number] || [];
-//         r[a.container.container_number].push(a);
-//         return r;
-//       }, Object.create(null));
-//       const newData = Object.values(container_data);
-//       console.log(newData);
-//       for (let i = 0; i < newData.length; i++) {
-//         const containerNumber = newData[i][0].container.container_number;
-//         containerNumberArr.push(containerNumber);
-//         containerMap.set(containerNumber, newData[i]);
-//         if (newData[i][0].container.status == '1') {
-//           receivedCount++
-//         } else if (newData[i][0].container.status == '2') {
-//           requestedCount++
-//         }
-//       }
-//     })
-//   };
-// allItem();
-
-
-
-// const chinaBoxTable = document.getElementById('myTable')
-// const tbody = chinaBoxTable.querySelectorAll('tbody');
-// tbody.forEach(i => i.remove());
--- public/amazon/javascript/home.js
+++ public/amazon/javascript/home.js
@@ -4,7 +4,7 @@ function number_item () {
     var table, tr, td, txtValue;
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
+    for (i = 1; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[6];
       if (td) {
         txtValue = td.textContent || td.innerText;
@@ -18,3 +18,52 @@ function number_item () {
   }
 setInterval (number_item(), 1000);
 document.getElementById('inventory_btn').click();
+/////////////////////
+const locationAddress = location.href.split('/');
+const account_id = locationAddress[locationAddress.length-1];
+const containerTable = document.getElementById('myTable');
+var containerMap = new Map();
+function allItem() {
+    fetch(`/api/item/allItem/${account_id}`, {
+      method: 'GET'
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      const container_data = data.reduce((r, a) => {
+        r[a.container.container_number] = r[a.container.container_number] || [];
+        r[a.container.container_number].push(a);
+        return r;
+      }, Object.create(null));
+      const newData = Object.values(container_data);
+      for (let i = 0; i < newData.length; i++) {
+        const containerNumber = newData[i][0].container.container_number;
+        containerMap.set(containerNumber, newData[i]);
+      };
+      var tr;
+      tr = containerTable.getElementsByTagName('tr');
+      for (let i = 1; i < tr.length; i++) {
+        const container_number = tr[i].getElementsByTagName('td')[1].innerHTML;
+        const sku = tr[i].getElementsByTagName('td')[2];
+        const qty = tr[i].getElementsByTagName('td')[3];
+        containerMap.get(container_number).forEach(item => {
+          const singleSKU = document.createElement('div');
+          const singleQty = document.createElement('div');
+          singleSKU.innerHTML = item.item_number;
+          singleQty.innerHTML = item.qty_per_sku;
+          sku.appendChild(singleSKU);
+          qty.appendChild(singleQty)
+        })
+      }
+    })
+  };
+allItem();
+
+
+
+
+
+
+
+// const chinaBoxTable = document.getElementById('myTable')
+// const tbody = chinaBoxTable.querySelectorAll('tbody');
+// tbody.forEach(i => i.remove());
