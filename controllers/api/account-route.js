194abc51faf5431d00a537af5379e6afeb1acae6
Rex Yu <yu19910513@gmail.com>
2022-04-15 10:31:43 -0700

    raw data inserted successfully

--- controllers/api/account-route.js
+++ controllers/api/account-route.js
@@ -5,7 +5,7 @@ const {withAuth, adminAuth} = require('../../utils/auth');
 //create new account
 router.post('/', withAuth, (req, res) => {
     Account.create({
-        user_id: 3,
+        user_id: req.session.user_id,
         name: req.body.name,
         prefix: req.body.prefix
     }).then(dbAccounttData => res.json(dbAccounttData))
