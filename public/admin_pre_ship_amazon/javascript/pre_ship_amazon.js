6c5169910ce81923eeb42e4dd16fbf01a1856dc9
Rex Yu <yu19910513@gmail.com>
2022-04-18 23:36:46 -0700

    checker

--- public/admin_pre_ship_amazon/javascript/pre_ship_amazon.js
+++ public/admin_pre_ship_amazon/javascript/pre_ship_amazon.js
@@ -39,6 +39,7 @@ function pre_check() {
             const qty = rows[i].getElementsByTagName("td")[1];
             qty.setAttribute('id',`qty_${sku.innerText}_${value}`)
         };
+        input.value = null;
     } else if (container_numberArr.includes(value) && document.getElementById(value).getAttribute('class') == 'lead text-center bg-success') {
         const eachTable = document.getElementById(`t_${value}`);
         const rows = eachTable.rows;
@@ -48,17 +49,27 @@ function pre_check() {
             const sku = rows[i].getElementsByTagName("td")[0];
             selectedSkuArr.push(sku.innerText);
         };
+        input.value = null;
     } else if (skuArr.includes(value) && !selectedSkuArr.includes(value) && document.getElementById(localStorage.getItem('selectedBox')).getAttribute('class') == 'lead text-center bg-success') {
-        const newQty = parseInt(document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML)-1;
-        if (newQty > 0) {
-            document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
-        } else if (newQty == 0) {
-            document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
-            document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).parentElement.setAttribute('class','bg-success');
-            selectedSkuArr.push(document.getElementById(`${value}_${localStorage.getItem('selectedBox')}`).innerHTML);
+        const qtyPerSKu = document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`);
+        if (qtyPerSKu) {
+            const newQty = parseInt(qtyPerSKu.innerHTML)-1;
+            if (newQty > 0) {
+                document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
+                input.value = null;
+            } else if (newQty == 0) {
+                document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
+                document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).parentElement.setAttribute('class','bg-success');
+                selectedSkuArr.push(document.getElementById(`${value}_${localStorage.getItem('selectedBox')}`).innerHTML);
+                input.value = null;
+            }
+        } else {
+            alert(`${value} is not associated with the box: ${localStorage.getItem('selectedBox')}; please scan the right box first`);
+            input.value = null;
         }
     } else {
-        alert('Incorrect Input')
+        alert('Incorrect Input');
+        input.value = null;
     }
 }
 
