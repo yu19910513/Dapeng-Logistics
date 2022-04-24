6fb04718cc4d1f02edf98ab04c5b92362c1fb3d1
Rex Yu <yu19910513@gmail.com>
2022-04-24 16:17:03 -0700

    new box pre ship

--- public/admin_pre_ship_amazon/javascript/pre_ship_amazon.js
+++ public/admin_pre_ship_amazon/javascript/pre_ship_amazon.js
@@ -2,6 +2,13 @@ const container_id = location.href.split('/').slice(-1)[0];
 const pre_shipN = document.getElementById('pre-shipN');
 const notes = document.getElementById('notes');
 const input = document.getElementById("scanned_item");
+const sku_list = document.getElementById('sku_list');
+const length = document.getElementById('new_len');
+const height = document.getElementById('new_hei');
+const weight = document.getElementById('new_wei');
+const width = document.getElementById('new_wid');
+const newContainerTable = document.getElementById('sku_table');
+
 var container_numberArr = [];
 function supplemental () {
     fetch(`/api/container/container/${container_id}`, {
@@ -26,7 +33,7 @@ var skuArr = [];
 function pre_check() {
     document.getElementById('alert').innerHTML = null;
     const value = input.value.trim();
-    const all_td = document.querySelectorAll('td');
+    const all_td = document.querySelectorAll('#pre_confirmTable td');
     if (container_numberArr.includes(value) && document.getElementById(value).getAttribute('class') != 'lead text-center rounded shadow-sm bg-info') {
         for (let i = 0; i < container_numberArr.length; i++) {
             document.getElementById(container_numberArr[i]).setAttribute('class', 'lead text-center rounded shadow-sm bg-light')
@@ -51,6 +58,7 @@ function pre_check() {
             rows[i].setAttribute('class','bg-info')
             const sku = rows[i].getElementsByTagName("td")[0];
             selectedSkuArr.push(sku.innerText);
+            eachBoxContent(rows[i])
         };
         input.value = null;
         td_checker (all_td);
@@ -60,8 +68,10 @@ function pre_check() {
             const newQty = parseInt(qtyPerSKu.innerHTML)-1;
             if (newQty > 0) {
                 document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
+                eachBoxContent(null, value)
                 input.value = null;
             } else if (newQty == 0) {
+                eachBoxContent(null, value)
                 document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).innerHTML = newQty;
                 document.getElementById(`qty_${value}_${localStorage.getItem('selectedBox')}`).parentElement.setAttribute('class','bg-info');
                 selectedSkuArr.push(document.getElementById(`${value}_${localStorage.getItem('selectedBox')}`).innerHTML);
@@ -129,6 +139,48 @@ function error() {
     var audio = new Audio('../media/wrong.mp3');
     audio.play();
 };
+function masterCheck () {
+    if (length.value && height.value && weight.value && width.value) {
+        document.getElementById('order_pre-check').style.display = '';
+        document.getElementById('fake').style.display = 'none';
+    } else {
+        document.getElementById('order_pre-check').style.display = 'none'
+        document.getElementById('fake').style.display = '';
+    }
+};
+var skuQtyMap = new Map();
+var tdSkuArr = [];
+function eachBoxContent (arr, input) {
+    if (arr) {
+        const newTr = document.createElement('tr');
+        const newBoxSku = document.createElement('td');
+        const newBoxQty = document.createElement('td');
+        sku_list.appendChild(newTr);
+        newTr.appendChild(newBoxSku);
+        newTr.appendChild(newBoxQty);
+        const tdPerRow = arr.querySelectorAll('td');
+        newBoxSku.innerHTML = tdPerRow[0].innerText;
+        newBoxQty.innerHTML = tdPerRow[1].innerText;
+    } else {
+        if(!tdSkuArr.includes(input)) {
+            tdSkuArr.push(input);
+            skuQtyMap.set(input, 1);
+            const newTr = document.createElement('tr');
+            const newBoxSku = document.createElement('td');
+            newBoxSku.setAttribute('id', input);
+            const newBoxQty = document.createElement('td');
+            newBoxQty.setAttribute('id', `${input}q`)
+            sku_list.appendChild(newTr);
+            newTr.appendChild(newBoxSku);
+            newTr.appendChild(newBoxQty);
+            newBoxSku.innerHTML = input;
+            newBoxQty.innerHTML = skuQtyMap.get(input)
+        } else {
+            skuQtyMap.set(input, skuQtyMap.get(input)+1)
+            document.getElementById(`${input}q`).innerHTML = skuQtyMap.get(input);
+        }
+    }
 
+}
 var instance = new Date().valueOf().toString().substring(5,13)+container_id;
 pre_shipN.innerHTML = `SP${instance}`
